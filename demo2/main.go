834ca2e2a96e683e7def5b478d403c15773141c6
chia <jnoideal@gmail.com>
2022-05-03 21:51:43 +0800

    second commit

---
+++ demo2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	var (
+		cmd    *exec.Cmd
+		output []byte
+		err    error
+	)
+
+	//generated cmd
+	cmd = exec.Command("/bin/bash", "-c", "sleep5; ls -l")
+
+	//The command was executed to capture the output of the child process(pipe)
+	if output, err = cmd.CombinedOutput(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Print child process output
+	fmt.Println(output)
+}
