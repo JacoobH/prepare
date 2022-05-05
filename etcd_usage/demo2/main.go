4a4d8ea2c218eb3a79d461cfa0a5139d9a16b420
chia <jnoideal@gmail.com>
2022-05-05 10:13:38 +0800

    #

---
+++ etcd_usage/demo2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/client/v3"
+	"time"
+)
+
+func main() {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		err     error
+		kv      clientv3.KV
+		putResp *clientv3.PutResponse
+	)
+	config = clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	}
+	//Establish a client
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+	//Use to read or write KV of etcd
+	kv = clientv3.NewKV(client)
+
+	if putResp, err = kv.Put(context.TODO(), "cron/jobs/job1", "hello"); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Revision:", putResp.Header.Revision)
+	}
+}
